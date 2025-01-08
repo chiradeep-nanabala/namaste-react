16c162a104e70041f57c7c5f161597a86ecfaf2a
Chiradeep Nanabala <chiradeepnanabala@Mac.attlocal.net>
2025-01-07 20:26:32 -0600

    episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 {/* <div id="parent">
     <div id="child">
         <h1> Sub Element h1 </h1>
@@ -14,7 +17,7 @@ const parent = React.createElement(
     "div",
     {id: "child"}, [
         React.createElement("h1", {}, "Sub Element h1"), 
-        React.createElement("h2", {}, "Sub Element h2")
+        React.createElement("h2", {}, "Sub Element h2 updated")
     ])  
 );
 
