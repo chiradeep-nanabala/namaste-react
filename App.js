87ddda0eae9370de83d259764062770d57bbb207
Chiradeep Nanabala <chiradeepnanabala@Mac.attlocal.net>
2025-01-15 13:24:21 -0600

    episode 3

--- App.js
+++ App.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-{/* <div id="parent">
-    <div id="child">
-        <h1> Sub Element h1 </h1>
-        <h2> Sub Element h2 </h2>
-    </div>
-</div> */}
-
 // ReactElement (Object) => HTML (Browser Understands)
 
-const parent = React.createElement(
-  "div",
-  {id: "parent"},
-  React.createElement(
-    "div",
-    {id: "child"}, [
-        React.createElement("h1", {}, "Sub Element h1"), 
-        React.createElement("h2", {}, "Sub Element h2 updated")
-    ])  
+// JSX => Babel transpiles it to React.createElement => React Element - JS Object => HTML Element(render)
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+
+const jsxHeading = (
+    <h1 className="head">
+        Namaste React using JSX
+    </h1>
 );
 
-const rootReact = ReactDOM.createRoot(document.getElementById("root"));
-rootReact.render(parent);
+const Title = () => (
+    <h1> This is the title </h1>
+);
+
+const SubHeading = () => (
+    <div class="subheading">
+        <h2> This is the subheading</h2>
+    </div>
+);
+
+// Component Composition
+const HeadingComponent = () => {
+    return (
+        <>
+            {jsxHeading}
+            <Title />
+            <Title></Title>
+            {Title()}
+            <SubHeading />
+            <h3>This is the last statement</h3>
+        </>
+    );
+    
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
+
